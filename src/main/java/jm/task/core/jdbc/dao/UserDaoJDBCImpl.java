e731a52fd981cae49e719359d9ad935b61dd54b2
DELL <arsenyev21@mail.ru>
2025-04-03 00:34:23 +0300

    Not Connection

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,40 +1,82 @@
 package jm.task.core.jdbc.dao;
 
-import com.sun.jdi.connect.spi.Connection;
+
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-
+    Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
     }
-
+    @Override
     public void createUsersTable() {
-
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS tabletwo" +
+                    "(id INT NOT NULL AUTO_INCREMENT,  firstName VARCHAR(40), lastName VARCHAR(40), age INT, PRIMARY KEY (id))");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
+    @Override
     public void dropUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+           try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO tabletwo(firstName, lastName, age) VALUES(?, ?, ?)")) {
+               preparedStatement.setString(1, name);
+               preparedStatement.setString(2, lastName);
+               preparedStatement.setByte(3, age);
+               preparedStatement.executeUpdate();
+           } catch (SQLException e) {
+               e.printStackTrace();
+           }
     }
-
+    @Override
     public void removeUserById(long id) {
+         try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
+             preparedStatement.setLong(1, id);
+             preparedStatement.executeUpdate();
+         } catch (SQLException e) {
+             e.printStackTrace();
 
+         }
     }
-
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try(Statement statement = connection.createStatement()) {
+           ResultSet resultSet = statement.executeQuery("SELECT * FROM tabletwo");
+           while (resultSet.next()) {
+               User user = new User();
+               user.setId(resultSet.getLong("id"));
+               user.setName(resultSet.getString("firstName"));
+               user.setLastName(resultSet.getString("lastName"));
+               user.setAge(resultSet.getByte("age"));
+               users.add(user);
+           }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
-
+    @Override
     public void cleanUsersTable() {
-
+       try (Statement statement = connection.createStatement()) {
+           statement.executeUpdate("DROP TABLE IF EXISTS users");
+       } catch (SQLException e) {
+           throw new RuntimeException(e);
+       }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,15 @@
 package jm.task.core.jdbc.util;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
 
-        private static final String URL = "jdbc:mysql://localhost:3306/your_database";
-        private static final String USER = "your_username";
-        private static final String PASSWORD = "your_password";
+    private static final String URL = "jdbc:mysql://localhost:3306/new_schema";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
 
     public static Connection getConnection() {
         Connection connection = null;
